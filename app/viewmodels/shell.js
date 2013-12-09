97b3969b487af30f50ad80ecea2dd458c7c787c2
Colin McCabe <illudens@gmail.com>
2013-12-09 07:31:24 -0600

    Commented out the extra guardRoute functionality

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -10,13 +10,15 @@
 	/////////////////////
 	
     // Redirecting from / to first route
-    router.guardRoute = function(routeInfo, params, instance){
+    /*
+	router.guardRoute = function(routeInfo, params, instance){
         if (params.fragment === ''){
             return routeInfo.router.routes[0].hash;
         }
         return true;
     };
-
+	*/
+	
 	/////////////////////
 	// Lifecyle Methods
 	/////////////////////
